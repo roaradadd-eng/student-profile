39e5bd987a0527802a66f749cecab33259c57aaf
roaradadd-eng <roaradadd@gmail.com>
2026-09-18 12:19:13 +0300

    Create student profile

---
+++ main.py
@@ -0,0 +1,16 @@
+# Student Profile - Ruba Ali Mohammad Raddad
+name = "رؤى علي محمد رداد"
+major = "علم حاسوب - فرع أمن سيبراني"
+university = "جامعة الزيتونة للعلوم والتكنولوجيا"
+graduation_year = "2028"
+hobby = "الرسم والتفكير في الأشياء الغريبة بطرق غريبة ومفيدة"
+career_goal = "تطوير الجانب العملي والمهني والتفكير القيادي والمهارات التقنية"
+programming_language = "Python"
+
+print(f"Name: {name}")
+print(f"Major: {major}")
+print(f"University: {university}")
+print(f"Graduation Year: {graduation_year}")
+print(f"Hobby: {hobby}")
+print(f"Career Goal: {career_goal}")
+print(f"Favorite Programming Language: {programming_language}")
